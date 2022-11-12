37beac4ba36f0d47fba53bebad322a4d1baf31d2
jonghun <jonghuncu@gmail.com>
2022-11-12 13:06:29 +0900

    Feat : update Post.java - Post 엔티티 기능 구현

---
+++ src/main/java/gdsc/blog/domain/Post.java
@@ -0,0 +1,27 @@
+package gdsc.blog.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data // Getter, Setter 생성
+@NoArgsConstructor // 파라미터가 없는 기본 생성자를 생성해줍니다.
+@AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자를 생성해줍니다.
+@Builder // builder 메서드를 자동 생성해줍니다.
+@Entity // 서버 실행 시 , Object Relation Mapping (ORM) 이 됨 (즉, 테이블이 DB에 생성 됨)
+public class Post {
+
+    @Id // PK를 해당 변수로 하겠다는 뜻.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // 해당 데이터베이스 번호 증가 전략을 따라가겠다 (H2 DB 전략을 따라감)
+    private Long id;
+
+    private String title;
+
+    @Column(length = 50000) // 최대 길이 설정
+    private String content;
+}
