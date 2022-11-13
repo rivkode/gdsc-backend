dfa917b03207e6cfb5cad3f0156ca67283628fa7
jonghun <jonghuncu@gmail.com>
2022-11-13 21:26:11 +0900

    Feat : update ErrorResponse.java - 예외처리 DTO

---
+++ src/main/java/gdsc/blog/dto/ErrorResponse.java
@@ -0,0 +1,18 @@
+package gdsc.blog.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+
+    private String code;
+
+    private String message;
+
+    @Builder
+    public ErrorResponse(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
