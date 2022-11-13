5e261d9cf05632ee9e794d7297c856381d73afdb
jonghun <jonghuncu@gmail.com>
2022-11-13 21:26:50 +0900

    Feat : update GlobalExceptionHandler.java - NoSuchElementException 처리

---
+++ src/main/java/gdsc/blog/advice/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package gdsc.blog.advice;
+
+import gdsc.blog.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // 특정 예외 클래스 발생시 실행되도록 설정
+    @ExceptionHandler({NoSuchElementException.class})
+    protected ResponseEntity<ErrorResponse> handleNosuchElementFoundException(NoSuchElementException e) {
+        final ErrorResponse errorResponse = ErrorResponse.builder()
+                .code("Item Not Found")
+                .message(e.getMessage()).build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+}
